90caa6141e0b5eb5da441c85d7c25e93b1625df8
Tanmay0405 <tanmayawasthi070@gmail.com>
2026-04-13 18:21:03 +0530

    Final backend setup and fixes before deployment

--- server/DB/conn.js
+++ server/DB/conn.js
@@ -1,27 +1,14 @@
 const mongoose = require("mongoose");
 
 const DB = process.env.DATABASE;
-mongoose.set('strictQuery', false)
-// try {
-//     mongoose.connect(DB, { useNewUrlParser: true, useUnfiedTopology: true }, () =>
-//       // console.log("conection successful ")
-//     );
-//   }
-//    catch (error) {
-//     // console.log(" no connection ");
-//   }
+
+mongoose.set("strictQuery", false);
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(DB, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      // usefindOneAndUpdate: false, // Use findOneAndUpdate() instead
-      // useCreateIndex: true // Use createIndex() instead
-    });
-    // console.log("MongoDB connected successfully");
+    await mongoose.connect(DB);
+    console.log("MongoDB connected successfully");
   } catch (error) {
-    // console.log(error)
     console.error("MongoDB connection error:", error.message);
     process.exit(1);
   }
--- server/app.js
+++ server/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+require("dotenv").config();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const cors = require("cors")
@@ -39,7 +40,7 @@ app.use(require("./router/hallRoutes"));
 connectDB()
 
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 5000;
 
 
 
